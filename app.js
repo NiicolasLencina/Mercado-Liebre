573437aa5068a03ed8abf609ea27286bdf8928f5
NiicolasLencina <Nicolas_lenciina@outlook.es>
2021-11-23 11:54:52 -0300

    cambios

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app.use(express.static('public'));
 
-app.get('/home', (req, res) => res.sendFile(__dirname +'/views/home.html'))
+app.get('/', (req, res) => res.sendFile(__dirname +'/views/home.html'))
 
 app.get('/registro', (req, res) => res.sendFile(__dirname +'/views/resgister.html'))
 
