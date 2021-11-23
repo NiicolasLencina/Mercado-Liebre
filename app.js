00c552d790a0cd9e7b8e64fa9932bb5035e60162
NiicolasLencina <Nicolas_lenciina@outlook.es>
2021-11-23 11:36:28 -0300

    Procfile creado

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/home', (req, res) => res.sendFile(__dirname +'/views/home.html'))
 
 app.get('/registro', (req, res) => res.sendFile(__dirname +'/views/resgister.html'))
 
+app.get('/registro2', (req, res) => res.sendFile(__dirname +'/views/resgister.html'))
+
 app.get('/login', (req, res) => res.sendFile(__dirname +'/views/login.html'))
 
-app.listen(port, () => console.log("FUNCIONA!!!"))
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
+});
